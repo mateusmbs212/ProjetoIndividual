677e3bac1b111b92ff7c426a85ebcc04c56df5d1
mateusmbs212 <matheus.mmsouza@sptech.school>
2022-06-05 16:13:57 -0300

    mudanças

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "JDMCars",
-    user: "aluno",
-    password: "sptech",
+    user: "root",
+    password: "Mateus30.",
 };
 
 function executar(instrucao) {
